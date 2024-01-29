bfb6ead56552ed8ef54ac8f5dd9c4a2fefbfcd62
hakull245 <125408638+HaTruong05@users.noreply.github.com>
2024-01-29 17:31:28 -0500

    Add getPlayerChoice()

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
-    let randomChoice = Math.floor(Math.random() * choices.length);
+    const randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
 }
 
+function getPlayerChoice() {
+    const choice = prompt("Choose Rock, Paper, or Scissors")
+    return choice[0].toUpperCase() + choice.substring(1).toLowerCase();
+}
+
+for (i=0; i<100; i++) {
+    console.log(getComputerChoice());
+}
