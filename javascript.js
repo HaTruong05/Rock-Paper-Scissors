16cbc0d01b0d7c4f9dfed9c2d6112e01f69335b2
hakull245 <125408638+HaTruong05@users.noreply.github.com>
2024-01-29 17:47:09 -0500

    Add playRound()

--- javascript.js
+++ javascript.js
@@ -9,6 +9,45 @@ function getPlayerChoice() {
     return choice[0].toUpperCase() + choice.substring(1).toLowerCase();
 }
 
-for (i=0; i<100; i++) {
-    console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    if (playerSelection === computerSelection) {
+        result = 'tie';
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            result = 'lose';
+        } else {
+            result = "win";
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Scissors") {
+            result = 'lose';
+        } else {
+            result = "win";
+        }
+    } else {
+        if (computerSelection === "Rock") {
+            result = "lose";
+        } else {
+            result = "win";
+        }
+    }
+
+    if (result === "lose") {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (result === "win") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `It's a Tie! You both chose ${playerSelection}`;
+    }
+}
+
+const tempChoices = ["Rock", "Paper", "Scissors"];
+for (i=0; i<15; i++) {
+    playerSelection = tempChoices[i%3];
+    console.log("Player chose" + playerSelection);
+    computerSelection = getComputerChoice();
+    console.log("Computer chose" + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
 }
